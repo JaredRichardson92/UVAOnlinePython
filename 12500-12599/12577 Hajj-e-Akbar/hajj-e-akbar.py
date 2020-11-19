43b9f482d90c7cec75cb4abebf4fd78f18fac954
Jared <Jared.Richardson@outlook.com>
2020-11-19 15:49:02 -0500

    Completed hajj-e-akbar.py.py for problem 12577

---
+++ 12500-12599/12577 Hajj-e-Akbar/hajj-e-akbar.py
@@ -0,0 +1,12 @@
+def main():
+    call = input()
+    case = 0
+    while call != '*':
+        case += 1
+        reply = "Akbar" if call == "Hajj" else "Asghar"
+        print("Case " + str(case) + ": Hajj-e-" + reply)
+        call = input()
+
+
+if __name__ == "__main__":
+    main()
