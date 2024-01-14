5dbec5acbfdafc945e41daa32be245762f241c0f
Jared Richardson <Jared.Richardson@outlook.com>
2024-01-13 19:39:35 -0500

    shorter code for 10019

--- 10000-10099/10019  Funny Encryption/encryption.py
+++ 10000-10099/10019  Funny Encryption/encryption.py
@@ -1,17 +1,9 @@
-def main():
-    numTests = int(input())
+numTests = int(input())
 
-    for _ in range(numTests):
-        n = int(input())
+for _ in range(numTests):
+    n = int(input())
 
-        binN = str(bin(n))[2:]
-        hexN = str(bin(int("0x" + str(n), 16)))[2:]
+    b1 = str((str(bin(n))[2:]).count("1"))
+    b2 = str((str(bin(int("0x" + str(n), 16)))[2:]).count("1"))
 
-        b1 = str(binN.count("1"))
-        b2 = str(hexN.count("1"))
-
-        print(b1 + " " + b2)
-
-
-if __name__ == "__main__":
-    main()
+    print(b1 + " " + b2)
